064ed7de1035384f6024837f3e1b667e79a52a4b
Sienna Kopf <princess.kopf@gmail.com>
2020-05-10 14:45:59 -0600

    Add independent challenge changes

--- lib/school.rb
+++ lib/school.rb
@@ -7,36 +7,34 @@ class School
     @start_time = start_time
     @hours_in_school_day = hours_in_school_day
     @student_names = []
-    @end_time = 0
   end
 
   def add_student_name(name)
     @student_names << name
   end
 
-  def end_time
-    @end_time = @start_time.to_i + @hours_in_school_day
-    "#{@end_time}:00"
+  def end_time ##Using a local varaible here instead of an instance one. Not super helpful to expose a variable like time to the rest of the methods when it is being referenced here.
+    time = @start_time.to_i + @hours_in_school_day
+    "#{time}:00"
   end
 
   def is_full_time?
-    if @hours_in_school_day > 4
-      true
-    end
+    @hours_in_school_day > 4 ## if statement is redundant and returns nil for non-full school days
   end
 
   def standard_student_names
-    @student_names.map do |student|
+    @student_names.map do |student| ## @ not necessary because of the method defined in the attr_reader
       student.capitalize
     end
   end
 
   def convert_end_time_to_clock_time
-    if @end_time> 12
-      pm_end_time = @end_time - 12
+    time = end_time.to_i
+    if time > 12
+      pm_end_time = time - 12
       "#{pm_end_time}:00"
     else
-      "#{@end_time}:00"
+      "#{time}:00"
     end
   end
 end
